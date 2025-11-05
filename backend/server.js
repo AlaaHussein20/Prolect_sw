4c21aa93520d5769e270273ef7ca3c886f381237
Alaa Hussein <A.hussein2393@nu.edu.eg>
2025-11-05 05:48:40 -0800

    add CORS middleware

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 const PORT = 5000;
+
+app.use(cors());
 
 // middleware
 app.use(express.json());
