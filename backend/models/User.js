38e7f4ce86885468d7486940f40ae5e579f99a87
Alaa Hussein <A.hussein2393@nu.edu.eg>
2025-11-05 06:54:14 -0800

    Enhance login page design with modern UI and animations

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,6 @@
 // models/User.js
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -21,5 +22,17 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 }, { timestamps: true });
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+// Method to compare password
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
 
 module.exports = mongoose.model('User', userSchema);
