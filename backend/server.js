782b6a2cd1ff348903df67b4c99d497d178bc0f6
Bassel Ali Azab <b.ali2346@nu.edu.eg>
2025-11-28 20:53:05 +0200

    update server port: change from 5000 to 5001

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const PORT = 5000;
+const PORT = 5001;
 
 app.use(cors());
 
